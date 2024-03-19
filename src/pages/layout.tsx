812c78b67b34e7561421977a87dad8166bcf9eb6
Yannik Sahl <me@yanniksahl.com>
2024-03-19 22:06:26 +0100

    add: particles

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,14 +1,18 @@
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
+import Particles from "@/components/layout/Particles";
 import { type PropsWithChildren } from "react";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-black">
-      <Header />
-      {children}
+    <div className="flex min-h-screen rotate-0 flex-col items-center justify-between overflow-hidden bg-black">
+      <Particles className="animate-fade-in absolute inset-0 -z-10" quantity={100} />
+      <main className="relative flex w-full max-w-5xl flex-1 flex-col items-center justify-center gap-10 py-16">
+        <Header />
+        {children}
+      </main>
       <Footer />
-    </main>
+    </div>
   );
 };
 
