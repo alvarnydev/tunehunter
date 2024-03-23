391d50174c5acc180dadd86a3dc49f20fbd4fd71
Yannik Sahl <me@yanniksahl.com>
2024-03-23 01:58:17 +0100

    added daisyui starters

--- src/components/interactive/ProfileButton.tsx
+++ src/components/interactive/ProfileButton.tsx
@@ -1,13 +1,26 @@
-import { User } from "lucide-react";
+import { User, X } from "lucide-react";
 import { Button } from "../ui/button";
 import ProfileModal from "../modals/ProfileModal";
+import { useState } from "react";
 
 const ProfileButton = () => {
+  const [profileModalOpen, setProfileModalOpen] = useState(false);
+
   return (
     <>
       {/* <ProfileModal /> */}
-      <Button variant="default" size="icon" className="w-12 h-12 bg-primary">
-        <User />
+      <Button
+        variant="default"
+        size="icon"
+        className="w-12 h-12 bg-primary relative"
+        onClick={() => setProfileModalOpen((open) => !open)}
+      >
+        <X
+          className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[1.6rem] w-[1.6rem] transition-all duration-500 ${profileModalOpen ? "rotate-0 scale-100" : "rotate-90 scale-0"}`}
+        />
+        <User
+          className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[1.6rem] w-[1.6rem] transition-all duration-500 ${profileModalOpen ? "-rotate-90 scale-0" : "rotate-0 scale-100"}`}
+        />
       </Button>
     </>
   );
--- src/components/interactive/ThemeToggle.tsx
+++ src/components/interactive/ThemeToggle.tsx
@@ -17,8 +17,8 @@ export function ThemeToggle() {
       onClick={handleThemeToggle}
       className=""
     >
-      <Sun className="h-[1.4rem] w-[1.4rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.4rem] w-[1.4rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <Sun className="h-[1.6rem] w-[1.6rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.6rem] w-[1.6rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
     </Button>
   );
 }
