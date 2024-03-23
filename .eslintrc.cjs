d0840a225e8c961f3f7e9da6a03fc77e80e49080
Yannik Sahl <me@yanniksahl.com>
2024-03-23 01:59:51 +0100

    new lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ const config = {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-empty-interface": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
