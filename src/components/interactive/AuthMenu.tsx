79589e4275ab702e543b091e0c994b4d7b28427c
Yannik Sahl <me@yanniksahl.com>
2024-05-01 19:38:24 +0200

    wip

--- src/components/interactive/AuthMenu.tsx
+++ src/components/interactive/AuthMenu.tsx
@@ -15,40 +15,38 @@ const AuthMenu: FC = ({}) => {
   const [menuState, setMenuState] = useState<MenuState>(0);
 
   return (
-    <>
-      <div className="relative w-full">
-        <div
-          className={cn(
-            "absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 justify-center transition-transform duration-500 ease-in-out",
-            menuState !== MenuState.SignIn && "-translate-x-[150%]",
-          )}
-        >
-          <div className={"w-fit"}>
-            <SignIn email={email} setEmail={setEmail} setMenuState={setMenuState} />
-          </div>
-        </div>
-        <div
-          className={cn(
-            "absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 justify-center transition-transform duration-500 ease-in-out",
-            menuState !== MenuState.MagicLinkSent && "translate-x-[150%]",
-          )}
-        >
-          <div className={"w-fit"}>
-            <MagicLinkSent email={email} />
-          </div>
-        </div>
-        <div
-          className={cn(
-            "absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 justify-center transition-transform duration-500 ease-in-out",
-            menuState !== MenuState.Register && "translate-x-[150%]",
-          )}
-        >
-          <div className={"w-fit"}>
-            <Register email={email} />
-          </div>
+    <div className="relative w-full">
+      <div
+        className={cn(
+          "absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 justify-center transition-transform duration-500 ease-in-out",
+          menuState !== MenuState.SignIn && "-translate-x-[150%]",
+        )}
+      >
+        <div className={"w-fit"}>
+          <SignIn email={email} setEmail={setEmail} setMenuState={setMenuState} />
         </div>
       </div>
-    </>
+      <div
+        className={cn(
+          "absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 justify-center transition-transform duration-500 ease-in-out",
+          menuState !== MenuState.MagicLinkSent && "translate-x-[150%]",
+        )}
+      >
+        <div className={"w-fit"}>
+          <MagicLinkSent email={email} />
+        </div>
+      </div>
+      <div
+        className={cn(
+          "absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 justify-center transition-transform duration-500 ease-in-out",
+          menuState !== MenuState.Register && "translate-x-[150%]",
+        )}
+      >
+        <div className={"w-fit"}>
+          <Register email={email} />
+        </div>
+      </div>
+    </div>
   );
 };
 
