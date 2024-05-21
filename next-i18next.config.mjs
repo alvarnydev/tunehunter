9e5aa9bef79c13e73feb6554fbeb0e78f5c0bfca
Yannik Sahl <me@yanniksahl.com>
2024-05-21 11:49:36 +0200

    sentry

--- next-i18next.config.mjs
+++ next-i18next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import("next-i18next").UserConfig} */
-const config = {
+const i18nConfig = {
   i18n: {
     locales: ["en", "de", "es"],
     defaultLocale: "en",
   },
 };
 
-export default config;
+export default i18nConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
+import { withSentryConfig } from "@sentry/nextjs";
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 
-import config from "./next-i18next.config.mjs";
-import { withAxiom } from "next-axiom";
+import i18nConfig from "./next-i18next.config.mjs";
 
 // @ts-check
 import("./src/env.js");
@@ -13,7 +13,7 @@ import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const NextConfig = {
   reactStrictMode: true,
-  i18n: config.i18n,
+  i18n: i18nConfig.i18n,
   images: {
     remotePatterns: [
       {
@@ -26,5 +26,44 @@ const NextConfig = {
   },
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-export default withAxiom(NextConfig);
+const sentryConfig = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  org: "alvarny-dev",
+  project: "tunehunter",
+
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Transpiles SDK to be compatible with IE11 (increases bundle size)
+  transpileClientSDK: true,
+
+  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+};
+
+const config = withSentryConfig(NextConfig, sentryConfig);
+
+export default config;
